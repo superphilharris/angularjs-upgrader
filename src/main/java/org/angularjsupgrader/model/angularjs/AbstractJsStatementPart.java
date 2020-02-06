323592dba62963274dbd11daf1484c55dc190816
philip <philip@the-harrises.com>
2020-02-07 07:03:35 +1300

    Ensure that we do not print out duplicated content when we have statements embedded inside statements

--- src/main/java/org/angularjsupgrader/model/angularjs/AbstractJsStatementPart.java
+++ src/main/java/org/angularjsupgrader/model/angularjs/AbstractJsStatementPart.java
@@ -4,5 +4,4 @@ package org.angularjsupgrader.model.angularjs;
  * Created by Philip Harris on 3/02/2020
  */
 public abstract class AbstractJsStatementPart {
-    public JsStatementBranch parent;
 }
--- src/main/java/org/angularjsupgrader/model/angularjs/JsStatement.java
+++
@@ -1,9 +0,0 @@
-package org.angularjsupgrader.model.angularjs;
-
-/**
- * Created by Philip Harris on 22/01/2020
- */
-public class JsStatement extends JsStatementBranch {
-    public String originalText;
-
-}
